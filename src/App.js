4cef60d027e59b1a8c1fe4be1b14851949ec30cd
Jan Trávníček <honza.travnicek85@gmail.com>
2023-01-28 21:28:53 +0100

    Projects added
    Project real text added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 
 import Header from "./components/layout/Header";
 import Content from "./components/layout/Content";
+import Mentions from "./components/layout/Mentions";
 
 function App() {
     return (
@@ -12,9 +13,7 @@ function App() {
             <div className="content">
                 <Content />
             </div>
-            <a href="https://www.flaticon.com/free-icons/school" title="school icons">
-                School icons created by Smashicons - Flaticon
-            </a>
+            <Mentions />
         </div>
     );
 }
---
+++ src/components/layout/Mentions.js
@@ -0,0 +1,14 @@
+
+import classes from "./css/Mentions.module.css";
+
+function Mentions(){
+    return (
+        <div className={classes.mentions}>
+            <a href="https://www.flaticon.com/free-icons/school" title="school icons">
+                School icons created by Smashicons - Flaticon
+            </a>
+        </div>
+    );
+}
+
+export default Mentions;
